b6bba8c0f722a42011af01033e37a2306da82ecd
Laura <laura-liis.parm@voco.ee>
2025-10-15 15:13:58 +0300

    Muutujad

---
+++ index.js
@@ -0,0 +1,22 @@
+/* 
+// Meetod, mis kuvab midagi konsoolis
+console.log("Hello World!");
+console.log("I like cats");
+// varjant 2
+M = "meetod, mis kuvab midagi veebilehel";
+console.log(M);
+
+Alerti meetod
+window.alert("This is an alert!");
+alert("This is also an alert!");
+*/
+
+document.getElementById("demo2").textContent = "This is a paragraph";
+document.getElementById("demo").textContent = "This is another paragraph";
+
+let age = 25;
+let price = 99.99;
+let gpa = 3.5;
+console.log(age);
+console.log(price);
+console.log("you are " + age + " years old");
