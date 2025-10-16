9e45544115b7fafe9669d7c216987a18e2e4fed7
Laura <laura-liis.parm@voco.ee>
2025-10-16 10:52:25 +0300

    first learning

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ Alerti meetod
 window.alert("This is an alert!");
 alert("This is also an alert!");
 */
-
+/*
 document.getElementById("demo2").textContent = "This is a paragraph";
 document.getElementById("demo").textContent = "This is another paragraph";
 
@@ -20,3 +20,43 @@ let gpa = 3.5;
 console.log(age);
 console.log(price);
 console.log("you are " + age + " years old");
+*/
+
+// Muutujad ja andmetüübid
+let firstName = "Laura-Liis";
+let lastName = "Pärm";
+let age = 23;
+let isStudent = true;
+
+document.getElementById("p1").textContent =
+  "My name is " + firstName + " " + lastName + ". I am " + age + " years old.";
+
+document.getElementById("p2").textContent = `You are ${firstName}`;
+
+/*
+Liitmine: +
+Lahutamine: -
+Korrutamine: *
+Jagamine: /
+Jääk: %
+Astendamine: **
+*/
+let result = 6 / 2 ** (2 + 5);
+console.log(result);
+
+// Kasutaja sisendi küsimine
+let userName = prompt("What is your name?");
+console.log(userName);
+document.getElementById(
+  "p3"
+).textContent = `Hello ${userName}, welcome to the website!`;
+
+let userName2;
+
+userName2 = window.prompt("What is your username?");
+console.log(userName2);
+
+document.getElementById("mySubmit").onclick = function () {
+  userName2 = document.getElementById("myText").value;
+  console.log(userName2);
+};
