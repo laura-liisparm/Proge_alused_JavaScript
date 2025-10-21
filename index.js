82abd880e7a5cc3219f4d88eada615d5b5e1263d
Laura <laura-liis.parm@voco.ee>
2025-10-21 11:54:49 +0300

    random nr generator

--- index.js
+++ index.js
@@ -1,62 +1,18 @@
-/* 
-// Meetod, mis kuvab midagi konsoolis
-console.log("Hello World!");
-console.log("I like cats");
-// varjant 2
-M = "meetod, mis kuvab midagi veebilehel";
-console.log(M);
+const myButton = document.getElementById("myButton");
+const label1 = document.getElementById("label1");
+const label2 = document.getElementById("label2");
+const label3 = document.getElementById("label3");
+const min = 1;
+const max = 6;
+let randomNum1;
+let randomNum2;
+let randomNum3;
 
-Alerti meetod
-window.alert("This is an alert!");
-alert("This is also an alert!");
-*/
-/*
-document.getElementById("demo2").textContent = "This is a paragraph";
-document.getElementById("demo").textContent = "This is another paragraph";
-
-let age = 25;
-let price = 99.99;
-let gpa = 3.5;
-console.log(age);
-console.log(price);
-console.log("you are " + age + " years old");
-*/
-
-// Muutujad ja andmetüübid
-let firstName = "Laura-Liis";
-let lastName = "Pärm";
-let age = 23;
-let isStudent = true;
-
-document.getElementById("p1").textContent =
-  "My name is " + firstName + " " + lastName + ". I am " + age + " years old.";
-
-document.getElementById("p2").textContent = `You are ${firstName}`;
-
-/*
-Liitmine: +
-Lahutamine: -
-Korrutamine: *
-Jagamine: /
-Jääk: %
-Astendamine: **
-*/
-let result = 6 / 2 ** (2 + 5);
-console.log(result);
-
-// Kasutaja sisendi küsimine
-let userName = prompt("What is your name?");
-console.log(userName);
-document.getElementById(
-  "p3"
-).textContent = `Hello ${userName}, welcome to the website!`;
-
-let userName2;
-
-userName2 = window.prompt("What is your username?");
-console.log(userName2);
-
-document.getElementById("mySubmit").onclick = function () {
-  userName2 = document.getElementById("myText").value;
-  console.log(userName2);
+myButton.onclick = function () {
+  randomNum1 = Math.floor(Math.random() * max) + min;
+  randomNum2 = Math.floor(Math.random() * max) + min;
+  randomNum3 = Math.floor(Math.random() * max) + min;
+  label1.textContent = randomNum1;
+  label2.textContent = randomNum2;
+  label3.textContent = randomNum3;
 };
