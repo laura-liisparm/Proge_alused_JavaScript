6ac2f301f3c5c7b2d18213ccd2f89d3c2f819888
Laura <laura-liis.parm@voco.ee>
2025-11-17 16:10:17 +0200

    camel

--- index.js
+++ index.js
@@ -1,45 +1,26 @@
-const scores = [45, 88, 32, 99, 74, 50, 61, 92, 39];
-let currentScores = [...scores]; // koopia
-
-const scoreList = document.getElementById("scoreList");
-const result = document.getElementById("result");
-
-const kpiCount = document.getElementById("kpi-count");
-const kpiAvg = document.getElementById("kpi-avg");
-const kpiTh = document.getElementById("kpi-th");
-
-const countInput = document.getElementById("Imput");
-
-//1.
-function renderScores() {
-  scoreList.innerHTML = "";
-  currentScores.forEach((score) => {
-    const item = document.createElement("p");
-    item.textContent = score;
-    scoreList.appendChild(item);
-  });
-  kpiCount.textContent = currentScores.length;
+function camelToTitleCase(words) {
+  words = words.trim().charAt(0).toUpperCase() + words.trim().slice(1);
+  let i = 0;
+  words = words.split("");
+  for (i; i < words.length; i += 1) {
+    if (
+      i > 0 &&
+      i < words.length - 1 &&
+      words[i] === words[i].toUpperCase() &&
+      (words[i - 1] != words[i - 1].toUpperCase() ||
+        words[i + 1] != words[i + 1].toUpperCase())
+    ) {
+      words.splice(i, 0, " ");
+      i += 1;
+    } else {
+    }
+  }
+  words = words.join("");
+  console.log(words);
 }
 
-//2.
-
-function addBonus() {
-  currentScores = currentScores.map((score) => score + 5);
-  renderScores();
-}
-
-//3.
-
-function filterByThreshold() {
-  currentScores = currentScores.filter((score) => score >= 60);
-  renderScores();
-}
-
-//4.
-
-function showAverage() {
-  let avg = Math.floor(
-    currentScores.reduce((acc, score) => (acc += score)) / currentScores.length
-  );
-  kpiAvg.textContent = avg;
-}
+camelToTitleCase("insertHTML");
+camelToTitleCase("insertAdjacentHTML");
+camelToTitleCase("firstHTMLElement");
+camelToTitleCase("releaseDate");
+camelToTitleCase("");
