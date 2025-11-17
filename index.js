009fd7a0679eca88e3585209db442d4cd1ab32e5
Laura <laura-liis.parm@voco.ee>
2025-11-17 16:05:13 +0200

    score_quest

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
-var a = parseInt(prompt("a = ?"));
-var b = parseInt(prompt("b = ?"));
+const scores = [45, 88, 32, 99, 74, 50, 61, 92, 39];
+let currentScores = [...scores]; // koopia
 
-newA = b;
-newB = a;
-document.getElementById("output").textContent =
-  "After swapping a = " + newA + " and b = " + newB;
+const scoreList = document.getElementById("scoreList");
+const result = document.getElementById("result");
+
+const kpiCount = document.getElementById("kpi-count");
+const kpiAvg = document.getElementById("kpi-avg");
+const kpiTh = document.getElementById("kpi-th");
+
+const countInput = document.getElementById("Imput");
+
+//1.
+function renderScores() {
+  scoreList.innerHTML = "";
+  currentScores.forEach((score) => {
+    const item = document.createElement("p");
+    item.textContent = score;
+    scoreList.appendChild(item);
+  });
+  kpiCount.textContent = currentScores.length;
+}
+
+//2.
+
+function addBonus() {
+  currentScores = currentScores.map((score) => score + 5);
+  renderScores();
+}
+
+//3.
+
+function filterByThreshold() {
+  currentScores = currentScores.filter((score) => score >= 60);
+  renderScores();
+}
+
+//4.
+
+function showAverage() {
+  let avg = Math.floor(
+    currentScores.reduce((acc, score) => (acc += score)) / currentScores.length
+  );
+  kpiAvg.textContent = avg;
+}
